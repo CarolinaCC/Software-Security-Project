0e35a58dd7ad8798b23e0de8d21678e0fce8670c
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-10 18:59:40 +0000

    Fix JSONS

--- source/pattern_parser.py
+++ source/pattern_parser.py
@@ -16,21 +16,26 @@ class Vulnerability:
         for sink in sinks:
             self.sinks.add(sink)
 
+    def __str__(self):
+        return str(self.sources) + "\n" + str(self.sanitizers) + "\n" + str(self.sinks)
+
+
+    def __repr__(self):
+        return str(self.sources) + "\n" + str(self.sanitizers) + "\n" + str(self.sinks)
+
 def parse(file_path: str) -> dict:
     # Loads json from file to variable
     with open(file_path, 'r') as f:
         patterns = json.load(f)
-        
+
     vulnerabilities = dict()
     for pattern in patterns:
         if not pattern['vulnerability'] in vulnerabilities:
             vulnerabilities[pattern['vulnerability']] = Vulnerability(pattern['sources'], pattern['sanitizers'], pattern['sinks'])
         else:
             vulnerabilities[pattern['vulnerability']].add(pattern['sources'], pattern['sanitizers'], pattern['sinks'])
-
     return vulnerabilities
 
 if __name__ == '__main__':
     import sys
     print(parse(sys.argv[1]))
-
