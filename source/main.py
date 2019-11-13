c65e49b37a0f6cb6c4a78c5bd8113b5b6bfbde07
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-13 17:24:40 +0000

    Begin parsing

--- source/main.py
+++ source/main.py
@@ -1,3 +1,12 @@
-'''
-Main Project File
-'''
+
+import ast
+import json
+import sys
+
+if __name__ == "__main__":
+    with open(sys.argv[1], 'r') as f:
+        tree = json.loads(f.read())
+    body = tree['body']
+    for stmt in body:
+        print(stmt)
+        print("\n\n")
