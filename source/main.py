06cb301324281d2b0edd1f84c7624a234f08e07d
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-25 21:47:28 +0000

    Recursive method

--- source/main.py
+++ source/main.py
@@ -5,11 +5,18 @@ import pattern_parser
 import ast_parser
 
 
+def prog_eval(patterns, prog, variables):
+    if prog == []:
+        return
+    prog[0].eval(variables, patterns)
+    prog_eval(patterns, prog[1:], variables)
+
 if __name__ == "__main__":
     patterns = pattern_parser.parse(sys.argv[1])
     prog = ast_parser.parse(sys.argv[2])
     variables = {}
-    for statement in prog:
-        statement.eval(variables, patterns)
+    prog_eval(patterns, prog, variables)
+    #for statement in prog:
+    #    statement.eval(variables, patterns)
     #for variable in variables:
         #print(f"variable {variable} : {variables[variable]}")
