e8343e9522f24bbfa1cc6d8e9f70e703568176d7
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-11-26 19:17:18 +0000

    added While Expression

--- source/main.py
+++ source/main.py
@@ -3,13 +3,23 @@ import sys
 import operator
 import pattern_parser
 import ast_parser
+import copy
 
 
 def prog_eval(patterns, prog, variables):
     if prog == []:
         return
-    prog[0].eval(variables, patterns)
-    prog_eval(patterns, prog[1:], variables)
+    if isinstance(prog[0], ast_parser.IfExpression):
+    	prog_eval(patterns, prog[0].body.append(prog[1:], copy.deepcopy(variables)))
+    	prog_eval(patterns, prog[0].else_body.append(prog[1:], copy.deepcopy(variables)))
+
+    if isinstance(prog[0], ast_parser.WhileExpression):
+    	# while body will run twice
+    	prog_eval(patterns, prog[0].body +  prog[0].body + prog[1:], copy.deepcopy(variables))
+    	prog_eval(patterns, prog[1:], copy.deepcopy(variables))
+    else:
+    	prog[0].eval(variables, patterns)
+    	prog_eval(patterns, prog[1:], variables)
 
 if __name__ == "__main__":
     patterns = pattern_parser.parse(sys.argv[1])
